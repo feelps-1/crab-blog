bf1b7cff200ce166ecfff86139cfe8a92d1e90df
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-03-15 10:57:41 -0300

    fix: big card responsiveness and styles

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  isHovered: boolean = false;
 
   @Input()
   photoCover:string =""
