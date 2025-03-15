6993842262be27dd5d5fd549b0a7e580bf6fa3fa
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-03-15 10:57:17 -0300

    fix: small card responsiveness and style

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  isHovered: boolean = false;
+
   @Input()
   photoCover:string = ""
 
